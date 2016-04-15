dc7a53494187135112d6b30b88ba9c5783ccfae2
Franklin van de Meent <fvdm@users.noreply.github.com>
2016-04-15 04:40:18 +0200

    Rewrite code to httpreq module

--- directvps.js
+++ directvps.js
@@ -11,134 +11,136 @@ Service URL:  https://directvps.nl
 Service API:  https://directvps.nl/api.pdf
 */
 
-var https = require ('https');
-var querystring = require ('querystring');
-var npath = require ('path');
+var http = require ('httpreq');
 var fs = require ('fs');
 
-module.exports = function (setup, errCallback) {
-  // Defaults
-  var set = {
-    timeout: 5000,
-    iface: setup.iface || null,
-    key: setup.key || null,
-    cert: setup.cert || null,
-    verify: setup.verify || false
+// Defaults
+var set = {
+  timeout: 5000,
+  iface: null,
+  key: null,
+  cert: null,
+  verify: true
+};
+
+
+/**
+ * Process response
+ * 
+ * @callback callback
+ * @param err {Error, null} - Request error
+ * @param res {Object} - Response data
+ * @param callback {function} - `function (err, data) {}`
+ * @returns {void}
+ */
+
+function doResponse (err, res, callback) {
+  var data = res && res.body || '';
+  var error = null;
+
+  try {
+    data = JSON.parse (data);
+
+    if (data [0] && data [0] .error && data [0] .error === '1') {
+      error = new Error('API error');
+      error.code = response.statusCode;
+      error.text = data[0].errormessage;
+      error.body = data;
+      doCallback (err);
+      return;
+    }
+
+    doCallback (null, data);
+  } catch (e) {
+    error = new Error ('not json');
+    error.body = data;
+    error.code = response.statusCode;
+    error.headers = response.headers
+    doCallback (error);
+  }
+}
+
+
+/**
+ * Communicate with API
+ * 
+ * @callback callback
+ * @param method {string} - HTTP method: GET, POST, PUT, DELETE
+ * @param path {string} - Request path after `/1`
+ * @param [params] {object} - Request query parameters or fields
+ * @param callback {function} - `function (err, data) {}`
+ * @returns {void}
+ */
+
+function talk (method, path, params, callback) {
+  var body = null;
+  var options = {
+    url: 'https://api.directvps.nl/1' + path,
+    method: method,
+    key: set.key,
+    cert: set.cert,
+    rejectUnauthorized: set.verify,
+    secureProtocol: 'TLSv1_method',
+    timeout: set.timeout,
+    headers: {
+      'Accept': 'application/json',
+      'User-Agent': 'directvps.js (https://github.com/fvdm/nodejs-directvps)'
+    }
   };
+
+  if (typeof params === 'function') {
+    callback = params;
+    params = null;
+  }
+
+  if (set.iface) {
+    options.rejectUnauthorized = set.iface;
+  }
+
+  if (method === 'POST') {
+    body = 'json=' + escape (JSON.stringify ([params]));
+    options.headers ['Content-Type'] = 'application/x-www-form-urlencoded';
+    options.headers ['Content-Length'] = body.length;
+  }
+
+  http.doRequest (options, function (err, res) {
+    doResponse (err, res || {}, callback);
+  });
+};
+
+
+/**
+ * Module interface
+ * 
+ * @param setup {object} - Configuration
+ * @param setup.key {string} - TLS private key or path to key
+ * @param setup.cert {string} - TLS certificate or path to certificate
+ * @param setup.verify {boolean=true} - Verify remote TLS certificate
+ * @param setup.iface {string} - Outbound network interface or IP-address
+ * @param setup.timeout {number=5000} - Wait time out in ms, 1000 = 1 sec
+ * @returns talk {function}
+ */
+
+module.exports = function (setup) {
+  var key;
+
+  for (key in setup) {
+    set [key] = setup [key];
+  }
 
   // Read files
   if (typeof set.key === 'string' && set.key.match (/^\//)) {
-    set.key = fs.readFileSync (set.key, {encoding: 'utf8'});
+    set.key = fs.readFileSync (set.key, {
+      encoding: 'utf8'
+    });
   }
 
   if (typeof set.cert === 'string' && set.cert.match (/^\//)) {
-    set.cert = fs.readFileSync (set.cert, {encoding: 'utf8'});
+    set.cert = fs.readFileSync (set.cert, {
+      encoding: 'utf8'
+    });
   }
 
   // API
-  return function (method, path, params, callback) {
-    if (typeof params === 'function') {
-      callback = params;
-      params = null;
-    }
-
-    // prevent multiple callbacks
-    var complete = false;
-    function doCallback (err, data) {
-      if (!complete) {
-        complete = true;
-        callback (err, data || null);
-      }
-    }
-
-    // build request
-    var options = {
-      host: 'api.directvps.nl',
-      path: '/1'+ path,
-      method: method,
-      key: set.key,
-      cert: set.cert,
-      rejectUnauthorized: set.verify,
-      secureProtocol: 'TLSv1_method',
-      headers: {
-        Accept: 'application/json',
-        'User-Agent': 'directvps.js (https://github.com/fvdm/nodejs-directvps)'
-      }
-    };
-
-    if (set.iface) {
-      options.rejectUnauthorized = set.iface;
-    }
-
-    var body = null;
-    if (method === 'POST') {
-      body = 'json='+ escape (JSON.stringify ([params]));
-      options.headers['Content-Type'] = 'application/x-www-form-urlencoded';
-      options.headers['Content-Length'] = body.length;
-    }
-
-    var request = https.request (options);
-
-    // Response
-    request.on ('response', function (response) {
-      var data = [];
-      var size = 0;
-
-      response.on ('data', function (ch) {
-        data.push (ch);
-        size += ch.length;
-      });
-
-      response.on ('close', function () {
-        doCallback (new Error ('request closed'));
-      });
-
-      response.on ('end', function () {
-        data = new Buffer.concat (data, size).toString ('utf8')
-        data = data.replace(/(\u0000)+$/, '').trim ();
-
-        try {
-          data = JSON.parse (data);
-
-          if (data[0] && data[0].error && data[0].error === '1') {
-            var error = new Error('API error');
-            error.code = response.statusCode;
-            error.text = data[0].errormessage;
-            error.body = data;
-            return doCallback (err);
-          }
-
-          doCallback (null, data);
-        }
-        catch (e) {
-          var err = new Error ('not json');
-          err.body = data;
-          err.code = response.statusCode;
-          err.headers = response.headers
-          doCallback (err);
-        }
-      });
-    });
-
-    // Timeout
-    request.on ('socket', function (socket) {
-      if (typeof set.timeout === 'number') {
-        socket.setTimeout (parseInt (set.timeout));
-        socket.on ('timeout', function () {
-          doCallback (new Error ('request timeout'));
-          request.abort ();
-        });
-      }
-    });
-
-    // Error
-    request.on ('error', function (e) {
-      var err = new Error ('request failed');
-      err.error = e;
-      doCallback (err);
-    });
-
-    // End
-    request.end (body);
-  };
+  return talk;
 }
